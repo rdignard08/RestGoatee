634716da07e8417210fdebeb6090738488085baa
Ryan Dignard <dignard@1debit.com>
2014-08-17 21:14:40 -0700

    added two test simple test cases

--- RestGoateeExampleTests/RestGoateeExampleTests.m
+++ RestGoateeExampleTests/RestGoateeExampleTests.m
@@ -8,6 +8,18 @@
 
 #import <XCTest/XCTest.h>
 
+@interface RGTestObject : NSObject
+
+@property (nonatomic, strong) NSString* string;
+@property (nonatomic, strong) NSArray* array;
+@property (nonatomic, strong) NSNumber* number;
+@property (nonatomic, assign) BOOL b;
+@property (nonatomic, assign) uint32_t value;
+@property (nonatomic, assign) Class type;
+
+@end
+@implementation RGTestObject @end
+
 @interface RestGoateeExampleTests : XCTestCase
 
 @end
@@ -26,9 +38,32 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void) testSerialization {
+    RGTestObject* testObject = [[RGTestObject alloc] init];
+    
+    testObject.string = @"world";
+    testObject.array = @[ @"abc" ];
+    testObject.number = @2;
+    testObject.value = 42;
+    
+    NSDictionary* serializedTest = [testObject dictionaryRepresentation];
+    
+    XCTest([serializedTest[@"string"] isEqual:@"world"], @"string failed");
+}
+
+- (void) testDeserialization {
+    NSDictionary* testJSON = @{
+                               @"_str_ing" : @"heelo!",
+                               @"array" : @[ @"1", @"2", @"3" ],
+                               @"number" : @"3",
+                               @"__b" : @YES,
+                               @"value" : @1214,
+                               @"type" : @"RGTestObject"
+                               };
+    
+    RGTestObject* test = [RGTestObject objectFromJSON:testJSON];
+    XCTest(test.b == YES, @"__b failed");
+    XCTest([test.string isEqual:testJSON[@"_str_ing"]], @"_str_ing failed");
 }
 
 @end
