1421fa0e8055c132b7d92561e8049878860d6730
Ryan Dignard <ryan@shoptap.it>
2014-12-26 00:22:01 -0800

    this macro is more useful as a block

--- RestGoatee/RestGoatee.h
+++ RestGoatee/RestGoatee.h
@@ -57,15 +57,21 @@ void rg_setServerTypeKey(const NSString* const typeKey);
  */
 const NSString* const rg_serverTypeKey(void);
 
-#define RISKY_BUSINESS(statement) \
+#define DO_RISKY_BUSINESS \
 _Pragma("clang diagnostic push") \
 _Pragma("clang diagnostic ignored \"-Warc-retain-cycles\"") \
 _Pragma("clang diagnostic ignored \"-Wgnu\"") \
 _Pragma("clang diagnostic ignored \"-Wunreachable-code\"") \
 _Pragma("clang diagnostic ignored \"-Wundeclared-selector\"") \
 _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-statement \
+
+#define END_RISKY_BUSINESS \
 _Pragma("clang diagnostic pop")
+
+#define RISKY_BUSINESS(statement) \
+DO_RISKY_BUSINESS \
+statement \
+END_RISKY_BUSINESS
 
 #import "NSObject+RG_KeyedSubscripting.h"
 #import "NSError+RG_HTTPStatusCode.h"
