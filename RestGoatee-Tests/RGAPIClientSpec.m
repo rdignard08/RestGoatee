5387b8dba646b5f8868111d73a700e60d6b34e11
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-31 02:22:01 -0800

    a test

---
+++ RestGoatee-Tests/RGAPIClientSpec.m
@@ -0,0 +1,27 @@
+//
+//  RestGoatee_Tests.m
+//  RestGoatee-Tests
+//
+//  Created by Ryan Dignard on 1/21/16.
+//  Copyright © 2016 Ryan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "RGResponseObject.h"
+#import "RGAPIClient.h"
+
+@interface RGAPIClientSpec : XCTestCase
+
+@end
+
+@implementation RGAPIClientSpec
+
+- (void) testGetSearch {
+    XCTestExpectation* expectation = [self expectationWithDescription:@""];
+    [[RGAPIClient manager] GET:@"https://itunes.apple.com/search" parameters:@{ @"term" : @"Pink Floyd" } keyPath:nil class:nil completion:^(RGResponseObject* response) {
+        [expectation fulfill];
+        XCTAssert(response.responseBody);
+    }];
+}
+
+@end
