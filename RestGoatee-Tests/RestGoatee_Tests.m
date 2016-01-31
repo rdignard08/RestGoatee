082a8d0761d69a9e7439d5837d89387e62628375
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-30 23:20:30 -0800

    remove example project

---
+++ RestGoatee-Tests/RestGoatee_Tests.m
@@ -0,0 +1,17 @@
+//
+//  RestGoatee_Tests.m
+//  RestGoatee-Tests
+//
+//  Created by Ryan Dignard on 1/21/16.
+//  Copyright © 2016 Ryan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RestGoatee_Tests : XCTestCase
+
+@end
+
+@implementation RestGoatee_Tests
+
+@end
