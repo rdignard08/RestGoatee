b0b65aaef008bc5d043db2746ac3e2147f3fefab
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-31 18:11:42 -0800

    what's going wrong

--- RestGoatee-Tests/RGAPIClientSpec.m
+++ RestGoatee-Tests/RGAPIClientSpec.m
@@ -20,6 +20,7 @@
     XCTestExpectation* expectation = [self expectationWithDescription:@""];
     [[RGAPIClient manager] GET:@"https://itunes.apple.com/search" parameters:@{ @"term" : @"Pink Floyd" } keyPath:nil class:nil completion:^(RGResponseObject* response) {
         [expectation fulfill];
+        NSLog(@"%@ %@ %@", response, response.responseBody, response.error);
         XCTAssert(response.responseBody);
     }];
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError* error) {
