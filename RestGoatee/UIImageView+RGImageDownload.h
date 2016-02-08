98f22cff0a5cdfa099cb8fa04656ea660c640171
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-02-07 21:56:25 -0800

    switch including mechanism

--- RestGoatee/UIImageView+RGImageDownload.h
+++ RestGoatee/UIImageView+RGImageDownload.h
@@ -21,7 +21,7 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-#import <AFNetworking/AFNetworking.h>
+#import "AFNetworking.h"
 #import "RestGoatee-Core.h"
 #import <UIKit/UIKit.h>
 
