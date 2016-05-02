41b79378528ea79c6926bb300c5cae5a4df24e1e
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-05-01 19:23:14 -0700

    Wrap long times in RGSerializationDelegate.h

--- RestGoatee/RGSerializationDelegate.h
+++ RestGoatee/RGSerializationDelegate.h
@@ -24,39 +24,43 @@
 #import <RestGoatee-Core/RestGoatee-Core.h>
 
 /**
- Certain requests to the API require additional information that might not be available in a standard request.
- 
- An `NSManagedObjectContext` is required for use with classes which are subclasses of `NSManagedObject`.
- 
- Unique checking can be performed here as well.
- 
- Finally you may enable XML parsing by implementing `-shouldSerializeXML` and returning `YES`.
+ @brief Certain requests to the API require additional information that might not be available in a standard request. An
+  `NSManagedObjectContext` is required for use with classes which are subclasses of `NSManagedObject`. Unique checking
+  can be performed here as well. Finally you may enable XML parsing by implementing `-shouldSerializeXML` and returning
+  `YES`.
  */
 @protocol RGSerializationDelegate <NSObject>
 
 @optional
 /**
- Implement this method if you wish to provide a context for response objects which are subclasses of `NSManagedObject`.  Types other than `NSManagedObject` are not queried.
+ @brief Implement this method if you wish to provide a context for response objects which are subclasses of
+  `NSManagedObject`.  Types other than `NSManagedObject` are not queried.
  */
 - (RG_PREFIX_NULLABLE NSManagedObjectContext*) contextForManagedObjectType:(RG_PREFIX_NONNULL Class)cls;
 
 /**
- Implement this method to retry requests as determined by you.  Return `YES` to retry; `NO` otherwise.  Default is `NO` for all requests.
- 
+ @brief Implement this method to retry requests as determined by you.  Return `YES` to retry; `NO` otherwise.  Default
+  is `NO` for all requests.
  @param request The request being retried. Contains the method, URL, and body as resolved by redirects.
- @param response The response that indicated failure.  Should be of type `NSHTTPURLResponse`, but you should type check anyway.
+ @param response The response that indicated failure.  Should be of type `NSHTTPURLResponse`, but you should type check
+  anyway.
  @param error The error that failed the request.
  @param count The number of times this request has been retried before.
  */
-- (BOOL) shouldRetryRequest:(RG_PREFIX_NONNULL NSURLRequest*)request response:(RG_PREFIX_NULLABLE NSURLResponse*)response error:(RG_PREFIX_NONNULL NSError*)error retryCount:(NSUInteger)count;
+- (BOOL) shouldRetryRequest:(RG_PREFIX_NONNULL NSURLRequest*)request
+                   response:(RG_PREFIX_NULLABLE NSURLResponse*)response
+                      error:(RG_PREFIX_NONNULL NSError*)error
+                 retryCount:(NSUInteger)count;
 
 /**
- Return a non-`nil` key to have `NSManagedObject`s be reconciled to an existing object if the value of this key matches.
+ @brief Return a non-`nil` key to have `NSManagedObject`s be reconciled to an existing object if the value of this key
+  matches.
  */
 - (RG_PREFIX_NULLABLE NSString*) keyForReconciliationOfType:(RG_PREFIX_NONNULL Class)cls;
 
 /**
- Enable basic XML to JSON parsing. If you want the `NSXMLParser` it will be passed back as the `responseBody` when `NO`.  Defaults to `NO`.
+ @brief Enable basic XML to JSON parsing. If you want the `NSXMLParser` it will be passed back as the `responseBody`
+  when `NO`.  Defaults to `NO`.
  */
 - (BOOL) shouldSerializeXML;
 
