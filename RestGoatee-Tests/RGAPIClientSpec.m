892b51a59eb12f39621322d01a9de8028a39c804
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-05-29 16:43:14 -0700

    test no completion

--- RestGoatee-Tests/RGAPIClientSpec.m
+++ RestGoatee-Tests/RGAPIClientSpec.m
@@ -66,20 +66,11 @@
     [[RGTapeDeck sharedTapeDeck] removeTapeForURL:@"https://itunes.apple.com/search"];
 }
 
-//- (void) testCallReal {
-//    XCTestExpectation* expectation = [self expectationWithDescription:@(sel_getName(_cmd))];
-//    RGAPIClient* client = [RGAPIClient manager];
-//    [client.recordedResponses removeObjectForKey:@"https://google.com/logout"];
-//    [client POST:@"https://google.com/logout" parameters:nil keyPath:nil class:Nil completion:^(RGResponseObject* response) {
-//        [expectation fulfill];
-//        XCTAssert(response.error);
-//    }];
-//    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError* error) {
-//        if (error) {
-//            XCTFail(@"Something went wrong.");
-//        }
-//    }];
-//}
+- (void) testWithoutCompletion {
+    RGAPIClient* client = [RGAPIClient manager];
+    [[RGTapeDeck sharedTapeDeck] playTape:@"itunes_search_json.txt" forURL:@"https://itunes.apple.com/search" withCode:200];
+    XCTAssertNoThrow([client GET:@"https://itunes.apple.com/search" parameters:@{ @"term" : @"Pink Floyd" } keyPath:nil class:Nil completion:nil]);
+}
 
 - (void) testXMLEndpoint {
     XCTestExpectation* expectation = [self expectationWithDescription:@(sel_getName(_cmd))];
