7c70dd07cf83beb38f51f4a84057e37cb461fb60
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-05-29 18:58:00 -0700

    test for hasChanges returns NO

--- RestGoatee-Tests/RGAPIClientSpec.m
+++ RestGoatee-Tests/RGAPIClientSpec.m
@@ -29,6 +29,7 @@
 #import "RGSerializationDelegate.h"
 #import "RGTapeDeck.h"
 #import "RGXMLTestObject.h"
+#import "NSManagedObjectContext+RGNeverHasChanges.h"
 
 @interface RGAPIClient (RGForwardDecl)
 
@@ -99,6 +100,31 @@
     }];
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError* error) {
         objc_setAssociatedObject(client, _cmd, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        if (error) {
+            XCTFail(@"Something went wrong.");
+        }
+    }];
+}
+
+- (void) testManagedObjectsWithDelegateNoChanges {
+    RGXMLTestObject* delegate = [RGXMLTestObject new];
+    delegate.wantsPrimaryKey = YES;
+    XCTestExpectation* expectation = [self expectationWithDescription:@(sel_getName(_cmd))];
+    RGAPIClient* client = [RGAPIClient manager];
+    client.serializationDelegate = delegate;
+    objc_setAssociatedObject(client, _cmd, delegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    rg_swizzle([NSManagedObjectContext self], @selector(hasChanges), @selector(override_hasChanges));
+    [[RGTapeDeck sharedTapeDeck] playTape:@"itunes_search_json.txt" forURL:@"https://itunes.apple.com/search" withCode:200];
+    [client GET:@"https://itunes.apple.com/search" parameters:@{ @"term" : @"Pink Floyd" } keyPath:@"results" class:[RGTestManagedObject self] completion:^(RGResponseObject* response) {
+        [expectation fulfill];
+        XCTAssert(response.responseBody.count == 1);
+        RGTestManagedObject* obj = response.responseBody.firstObject;
+        XCTAssert([obj.trackId isEqual:@"1065976170"]);
+        XCTAssert([obj.trackName isEqual:@"Comfortably Numb"]);
+    }];
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError* error) {
+        objc_setAssociatedObject(client, _cmd, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        rg_swizzle([NSManagedObjectContext self], @selector(hasChanges), @selector(override_hasChanges));
         if (error) {
             XCTFail(@"Something went wrong.");
         }
