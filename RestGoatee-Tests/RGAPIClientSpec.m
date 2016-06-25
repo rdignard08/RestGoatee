30b76363ca788572917d26b0c30b56c6a001b502
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-06-24 20:34:09 -0700

    try one without swizzling hasChanges

--- RestGoatee-Tests/RGAPIClientSpec.m
+++ RestGoatee-Tests/RGAPIClientSpec.m
@@ -237,7 +237,6 @@
     client.serializationDelegate = delegate;
     client.responseSerializer = [AFXMLParserResponseSerializer new];
     objc_setAssociatedObject(client, _cmd, delegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    rg_swizzle([NSManagedObjectContext self], @selector(hasChanges), @selector(override_hasChanges));
     [[RGTapeDeck sharedTapeDeck] playTape:@"itunes_search_as_xml.txt" forURL:@"https://itunes.apple.com/search" withCode:200];
     [client GET:@"https://itunes.apple.com/search"
      parameters:@{ @"term" : @"Pink Floyd" }
@@ -255,7 +254,6 @@
     }];
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError* error) {
         objc_setAssociatedObject(client, _cmd, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        rg_swizzle([NSManagedObjectContext self], @selector(hasChanges), @selector(override_hasChanges));
         if (error) {
             XCTFail(@"Something went wrong.");
         }
