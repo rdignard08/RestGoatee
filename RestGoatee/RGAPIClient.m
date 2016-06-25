56d14d42b2be84dff0608f688a0fe5f35b0d47da
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-06-24 21:30:33 -0700

    remove check and replace with assertion

--- RestGoatee/RGAPIClient.m
+++ RestGoatee/RGAPIClient.m
@@ -88,8 +88,7 @@ static inline NSError* errorWithStatusCodeFromTask(NSError* error, NSURLResponse
     }
     NSArray* target = path ? [response valueForKeyPath:path] : response;
     target = !target || [target isKindOfClass:[NSArray class]] ? target : @[ target ];
-    if (primaryKey &&
-        [cls isSubclassOfClass:kRGNSManagedObject]) {
+    if (primaryKey && [cls isSubclassOfClass:kRGNSManagedObject]) {
         NSObject* fetch = [objc_getClass("NSFetchRequest") fetchRequestWithEntityName:NSStringFromClass(cls)];
         NSArray* incomingKeys = [target valueForKey:primaryKey];
         NSMutableArray* parsedKeys = [NSMutableArray arrayWithCapacity:incomingKeys.count];
@@ -109,7 +108,11 @@ static inline NSError* errorWithStatusCodeFromTask(NSError* error, NSURLResponse
     for (NSUInteger i = 0; i < target.count; i++) {
         id entry = target[i];
         BOOL dataSrc = [entry isKindOfClass:[NSDictionary self]] || [entry conformsToProtocol:@protocol(RGDataSource)];
-        if (dataSrc && primaryKey && allObjects && [entry valueForKey:primaryKey]) {
+        if (dataSrc && primaryKey && allObjects) {
+            NSAssert([entry valueForKey:primaryKey],
+                     @"entry %@ did not provide a value for primaryKey %@",
+                     entry,
+                     primaryKey);
             id entryKey = [entry valueForKey:primaryKey];
             id keyValue = entryKey;
             if ([entry isKindOfClass:[RGXMLNode self]]) {
