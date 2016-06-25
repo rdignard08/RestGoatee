92c268bf7232dd8966a04ace7bc60fb0c4c2351f
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-06-24 19:49:37 -0700

    don't need to wrap these now

--- RestGoatee/RGAPIClient.m
+++ RestGoatee/RGAPIClient.m
@@ -108,9 +108,7 @@ static inline NSError* errorWithStatusCodeFromTask(NSError* error, NSURLResponse
         }];
     }
     NSMutableArray* ret = [NSMutableArray arrayWithCapacity:[target count]];
-    for (NSUInteger i = 0;
-         i < target.count;
-         i++) {
+    for (NSUInteger i = 0; i < target.count; i++) {
         id entry = target[i];
         if (([entry isKindOfClass:[NSDictionary class]] ||
              [entry conformsToProtocol:@protocol(RGDataSource)]) &&
