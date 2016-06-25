92de068cd67f575e8333479858f797a17d393aca
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-06-25 01:45:07 -0700

    primaryKey is only set when this is true

--- RestGoatee/RGAPIClient.m
+++ RestGoatee/RGAPIClient.m
@@ -88,7 +88,7 @@ static inline NSError* errorWithStatusCodeFromTask(NSError* error, NSURLResponse
     }
     NSArray* target = path ? [response valueForKeyPath:path] : response;
     target = !target || [target isKindOfClass:[NSArray class]] ? target : @[ target ];
-    if (primaryKey && [cls isSubclassOfClass:kRGNSManagedObject]) {
+    if (primaryKey) {
         NSObject* fetch = [objc_getClass("NSFetchRequest") fetchRequestWithEntityName:NSStringFromClass(cls)];
         NSArray* incomingKeys = [target valueForKey:primaryKey];
         NSMutableArray* parsedKeys = [NSMutableArray arrayWithCapacity:incomingKeys.count];
