e77cbf4e7b1bcdf1053efc1392bff06b2f860c43
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-09-01 23:18:13 -0700

    don't depend on implicit Foundation

--- RestGoateeFramework/RestGoateeFramework.h
+++ RestGoateeFramework/RestGoateeFramework.h
@@ -21,6 +21,8 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
+#import <Foundation/Foundation.h>
+
 /**
  Project version number for RestGoateeFramework.
  */
